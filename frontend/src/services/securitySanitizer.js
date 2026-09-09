bb8adc12e8adb53b1be3a16965bf5d86c34997a4
Nagaphanisree Meesala <23471a4392@gmail.com>
2026-09-09 17:04:14 +0530

    feat(security): create securitySanitizer helper with HTML escaping

---
+++ frontend/src/services/securitySanitizer.js
@@ -0,0 +1,4 @@
+export const sanitizeInput = (str = '') => {
+  if (typeof str !== 'string') return str;
+  return str.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&#x27;');
+};
